2caa5f2ff78d9ecddc01ab04bf45df4d743e01c8
Marko Hlebar <marko.hlebar@clover-studio.com>
2013-11-10 11:37:10 +0100

    wrapped json_junk in a class, added gitignore

--- json-junk.rb
+++ json-junk.rb
@@ -1,8 +1,14 @@
 require "rubygems"
 require "json"
 
-json_file_path, options = ARGV
+json_file_path, suffix, junk = ARGV
 
+if not json_file_path 
+	puts "Missing file path to the .json file"
+	abort() 
+end	
+
+suffix == nil ? "-junk" : suffix
 
 #adds a suffix to a file path, i.e. file_path.sh = file_path-suffix.sh
 def add_suffix(file_path, suffix) 
@@ -13,50 +19,58 @@ def is_collection(collection)
 	collection.respond_to?'each'
 end 
 
-$array_count = 0
-
-def recurse_collection_and_nullify_leafs(collection)
-# 	puts collection
+class JsonJunk 
+	def initialize(json_file_path, export_suffix, junk) 
+		@json_file_path = json_file_path
+		@export_suffix = export_suffix
+		@junk = junk
+	end
 	
-	modified_collection = collection.clone()
-	#check if leaf is a collection
-	if modified_collection.is_a?(Hash)
-		modified_collection.each do |key, value|
-			if is_collection(value)
-				modified_collection[key] = recurse_collection_and_nullify_leafs(value)
-			else 
-				modified_collection[key] = nil
+	def junkify()
+# 		puts @json_file_name
+# 		puts @export_suffix
+		export_file_path = add_suffix(@json_file_path, @export_suffix)
+		parsed_object = JSON.parse(File.read(@json_file_path))
+		
+		if parsed_object.is_a?(Hash)
+		    modified_hash = recurse_collection_and_junkify_leafs(parsed_object, @junk)
+		    json_string = JSON.pretty_generate(modified_hash)
+		    File.open(export_file_path, 'w').write(json_string)
+		end 
+	end 
+
+	def recurse_collection_and_junkify_leafs(collection, junk)	
+		modified_collection = collection.clone()
+		#check if leaf is a collection
+		if modified_collection.is_a?(Hash)
+			modified_collection.each do |key, value|
+				if is_collection(value)
+					modified_collection[key] = recurse_collection_and_junkify_leafs(value, junk)
+				else 
+					modified_collection[key] = junk
+				end	
 			end	
-		end	
-	elsif modified_collection.is_a?(Array)
-		$array_count += 1
-		puts modified_collection
-		puts "Start array #{$array_count}" 
-		modified_collection.each do |value| 
-			new_value = nil
-			if is_collection(value) 
-				new_value = recurse_collection_and_nullify_leafs(value)
-			end
-			puts "value = #{value}\n#########\nnewValue = #{new_value}\n$$$$$$$$$$$$"
-			modified_collection.push(new_value)
-			modified_collection.delete(value)
-		end	
-		puts "END"
-	else 
-#  		puts modified_collection
-	end			
-	modified_collection
-end  	  
+		elsif modified_collection.is_a?(Array)
+			array = Array.new()
+			modified_collection.each do |value| 
+				new_value = junk
+				if is_collection(value) 
+					new_value = recurse_collection_and_junkify_leafs(value, junk)
+				end
+				array.push(new_value)
+			end	
+			#store all modified values back to modified collection 		
+			modified_collection = array.clone()
+			else 
+		end			
+		modified_collection
+	end  	  
+end
 
-export_file_path = add_suffix(json_file_path, "-junk")
-parsed_object = JSON.parse(File.read(json_file_path))
 
-if parsed_object.is_a?(Hash)
-    modified_hash = recurse_collection_and_nullify_leafs(parsed_object["metadata"]["results"][0])
-#     modified_hash = recurse_collection_and_nullify_leafs(parsed_object)
-    json_string = JSON.pretty_generate(modified_hash)
-    File.open(export_file_path, 'w').write(json_string)
-end 
+json_junk = JsonJunk.new(json_file_path, suffix, junk)
+json_junk.junkify()
+
 
 
 
